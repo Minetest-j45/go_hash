c5cdb7a8f92c0374da2bf0e174f78e62a03a9e5e
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-14 17:27:20 +0100

    Update init.go

--- init.go
+++ init.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"log"
 	"os"
   	"fmt"
 	"github.com/tncardoso/gocurses"
+	"strings"
 )
 
 // a rune is actually just an int32 but we need to change the variable type in order to be able to pass it to functions etc.
 
 func main() {
-    str := "str to int conv" // string to convert
-    ra := []rune(str)        // convert it to a rune array
-    var ia []int32           // empty int32 array to store the result in
-    for _, v := range ra {   // loop over every element of the rune array
-        ia = append(ia, int32(v)) // convert the rune into int32 (just a type update, the data doesn't change, rune is just int32 with some methods) and append it to the result array
-    }
+	if len(os.Args) < 2 {
+		log.Fatal("Not enough arguments!")
+	}
 
-    fmt.Println(ia) // prints [115 116 114 32 116 111 32 105 110 116 32 99 111 110 118] you can iterate this and perform the % operation
+	input := strings.Join(os.Args[1:], " ") 
+	str := "str to int conv" // string to convert
+	ra := []rune(str)        // convert it to a rune array
+	var ia []int32           // empty int32 array to store the result in
+	for _, v := range ra {   // loop over every element of the rune array
+		ia = append(ia, int32(v)) // convert the rune into int32 (just a type update, the data doesn't change, rune is just int32 with some methods) and append it to the result array
+	}
+
+    	fmt.Println(ia) // prints [115 116 114 32 116 111 32 105 110 116 32 99 111 110 118] you can iterate this and perform the % operation
 }
