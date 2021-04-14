29c2d8e663ce0cab7fff99b2ceb5af5d1504358f
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-14 17:45:17 +0100

    Update init.go

--- init.go
+++ init.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"log"
-	"os"
-  	"fmt"
-	"github.com/tncardoso/gocurses"
-	"strings"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+
+    "github.com/tncardoso/gocurses"
 )
 
 // a rune is actually just an int32 but we need to change the variable type in order to be able to pass it to functions etc.
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Not enough arguments!")
-	}
+    if len(os.Args) < 2 {
+        log.Fatal("Not enough arguments!")
+    }
 
-	input := strings.Join(os.Args[1:], " ") 
-	ra := []rune(input)        // convert it to a rune array
-	var ia []int32           // empty int32 array to store the result in
-	for _, v := range ra {   // loop over every element of the rune array
-		ia = append(ia, int32(v)) // convert the rune into int32 (just a type update, the data doesn't change, rune is just int32 with some methods) and append it to the result array
-	}
+    input := strings.Join(os.Args[1:], " ") 
+    ra := []rune(input)        // convert it to a rune array
+    var ia []int32           // empty int32 array to store the result in
+    for _, v := range ra {   // loop over every element of the rune array
+        ia = append(ia, int32(v)) // convert the rune into int32 (just a type update, the data doesn't change, rune is just int32 with some methods) and append it to the result array
+    }
 
-    	fmt.Println(ia) // prints [115 116 114 32 116 111 32 105 110 116 32 99 111 110 118] you can iterate this and perform the % operation
-	
-	var ib []int32
-	for _, int32 := range ia {
-		ib = append(ib, int32 % 7)
-	}
-	
-	fmt.Println(ib)
+        fmt.Println(ia) // prints [115 116 114 32 116 111 32 105 110 116 32 99 111 110 118] you can iterate this and perform the % operation
+    
+    for i := range ia {
+        ia[i] %= 7
+    }
 }
